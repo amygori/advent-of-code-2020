569dc3f92217f8c3d3e142730293612f063d33bc
Amy Gori <amydgori@gmail.com>
2020-12-06 16:33:17 -0500

    Clean up day 5 a little

--- day-5/day5.py
+++ day-5/day5.py
@@ -3,12 +3,32 @@ def run(file):
     with open(file) as file:
         seat_codes = file.readlines()
         seat_codes = [seat_code.strip() for seat_code in seat_codes]
-        print(seat_codes)
         seats = []
         for seat_code in seat_codes:
             seats.append(decode(seat_code))
         print(f"The highest seat number is {max(seats)}")
         find_my_seat(seats)
+
+
+def decode(seat_code):
+    row = parse_chars(seat_code[:-3])
+    column = parse_chars(seat_code[-3:])
+    seat_number = find_seat_number(row, column)
+    return seat_number
+
+
+def parse_chars(code):
+    start = 0
+    stop = 127 if 'F' in code or 'B' in code else 7
+    for char in code:
+        take_lower = char == 'F' or char == 'L'
+        take_upper = char == 'B' or char == 'R'
+        midpoint = (stop + start) // 2
+        if take_lower:
+            stop = midpoint
+        if take_upper:
+            start = midpoint
+    return stop
 
 
 def find_my_seat(seats):
@@ -19,48 +39,8 @@ def find_my_seat(seats):
             print(f"The missing seat number is {num}")
 
 
-def decode(seat_code):
-    row_code = seat_code[:-3]
-    column_code = seat_code[-3:]
-    row = parse_row(row_code)
-    column = parse_column(column_code)
-    seat_number = find_seat_number(row, column)
-    return seat_number
-
-
-def parse_row(row_code):
-    start = 0
-    stop = 127
-    for char in row_code:
-        diff = (stop + start) // 2
-        if char == 'F' or char == 'R':
-            stop = diff
-            # print(f"F means take lower: keep rows {start} to {stop}")
-        if char == 'B' or char == 'L':
-            start = diff + 1
-            # print(f"B means take upper: keep rows {start} to {stop}")
-    # print(f"Parse row: {stop}")
-    return stop
-
-
-def parse_column(column_code):
-    start = 0
-    stop = 7
-    for char in column_code:
-        diff = (stop + start) // 2
-        if char == 'F' or char == 'L':
-            stop = diff
-            # print(f"L means take lower: keep rows {start} to {stop}")
-        if char == 'B' or char == 'R':
-            start = diff + 1
-            # print(f"R means take upper: keep rows {start} to {stop}")
-    # print(f"Parse column: {stop}")
-    return stop
-
-
 def find_seat_number(row, column):
     seat_number = (int(row) * 8) + int(column)
-    print(f"Seat number is {seat_number}")
     return seat_number
 
 
@@ -69,7 +49,7 @@ if __name__ == "__main__":
     from pathlib import Path
 
     parser = argparse.ArgumentParser(
-        description='Not very binary Binary Partioning for Advent of Code 2020 Day 4.')
+        description='Not very binary Binary Partioning for Advent of Code 2020 Day 5.')
     parser.add_argument('file', help='file to read')
     args = parser.parse_args()
 
