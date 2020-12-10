c3c2b89b05c2eb8e529f98c7951351f74a0178c6
Amy Gori <amydgori@gmail.com>
2020-12-10 16:32:18 -0500

    Day 6 part 2

--- day-6/day6.py
+++ day-6/day6.py
@@ -1,14 +1,25 @@
-import itertools
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
-
-
 def run(file):
     with open(file) as file:
         lines = [line.strip() for line in file.readlines()]
-        data = format_data(lines)
-        count_answers(data)
+        calculate_all_yes_answers(process_data(lines))
+
+
+def process_data(lines):
+    data = []
+    answer_group = []
+    answer_set = set()
+    for line in lines:
+        if line:
+            # keep adding to the answer group
+            answer_group.append(set(line))
+            if line == lines[-1]:
+                data.append(answer_group)
+        else:
+            # then we want to add the temp data var to a data list
+            data.append(answer_group)
+            # and reset the group variable
+            answer_group = []
+    return data
 
 
 def format_data(lines):
@@ -28,8 +39,17 @@ def format_data(lines):
     return data
 
 
+def calculate_all_yes_answers(data):
+    answers_count = 0
+    for answer_group in data:
+        intersection = set.intersection(*answer_group)
+        answers_count += len(set.intersection(*answer_group))
+    print(f"YES Answers count: {answers_count}")
+
+
 def count_answers(data):
     count = []
+    print(data)
     for answer_set in data:
         unique_chars = list(set(answer_set))
         count.append(len(unique_chars))
