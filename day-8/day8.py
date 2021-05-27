6ca8e06e516435edd51cd83a563294f88144e739
Amy Gori <amydgori@gmail.com>
2021-05-26 22:18:37 -0400

    Day 8 part 2

--- day-8/day8.py
+++ day-8/day8.py
@@ -1,27 +1,26 @@
 from functools import partial
+import sys
+sys.setrecursionlimit(10000)
 
 accumulator = 0
 lines_index = []
 lines = []
-done = False
+start_loop = False
+swap_history = []
+already_swapped = False
 
 
 def tick_accumulator(num, index):
-    print(f"hello from the accumulator, adding {num}")
     global accumulator
     accumulator += eval(num)
-    print(f"Now accumulator is: ", accumulator)
     parse_code(index+1)
 
 
 def skip_to_line(jump, index):
-    print(f"SKIP_TO_LINE, index is: ", index)
-    print(f"And jump is: ", jump)
     parse_code(index+eval(jump))
 
 
 def do_nothing(_, index):
-    print(f"DO_NOTHING. index is:", index)
     parse_code(index+1)
 
 
@@ -36,39 +35,76 @@ def run(file):
     global lines
     with open(file) as file:
         lines = [line.strip() for line in file.readlines()]
-    print("Lines:", lines)
-    parse_code(0)
+    parse_code()
+
+
+def swap_instructions(operation):
+    swap = {
+        'jmp': 'nop',
+        'nop': 'jmp'
+    }
+    global already_swapped
+    already_swapped = True
+    return swap[operation]
+
+
+def log_swap(idx):
+    global swap_history
+    swap_history.append(idx)
 
 
 def parse_code(index=0):
-    print("PARSE_CODE, index: ", index)
-    global lines
-    check_lines(index)
-    if done:
-        print(f"Here is the value in the accumulator: {accumulator}")
+    global accumulator
+    global start_loop
+    check_if_seen(index)
+    if index == len(lines):
+        print(
+            f"Value in the accumulator at the end: {accumulator}")
+        return
+    if start_loop:
+        restart()
     else:
         execute_instructions(index)
 
 
+def restart():
+    reset_global_values()
+    parse_code()
+
+
+def reset_global_values():
+    global accumulator
+    accumulator = 0
+    global lines_index
+    lines_index = []
+    global start_loop
+    start_loop = False
+    global already_swapped
+    already_swapped = False
+
+
 def execute_instructions(index):
+    global already_swapped
+    global lines
+    global swap_history
+    global lines_index
     operation = lines[index][:3]
+    if not already_swapped:
+        if (operation != 'acc') and (index not in swap_history):
+            operation = swap_instructions(operation)
+            log_swap(index)
     value = lines[index][4:]
     OPERATIONS[operation](value, index)
 
 
-def check_lines(index):
-    print("CHECK_LINES, index: ", index)
-    global lines
+def check_if_seen(index):
     global lines_index
-    global done
-    if not lines:
-        return False
-    index_of_instruction = index
-    if index_of_instruction in lines_index:
-        done = True
+    global start_loop
+    current_index = index
+    if current_index in lines_index:
+        start_loop = True
     else:
-        lines_index.append(index_of_instruction)
-    print(f"Lines index: ", lines_index)
+        lines_index.append(current_index)
 
 
 if __name__ == "__main__":
